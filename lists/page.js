3ca54236f813403c73f39be529308c55e1276578
Benjamin Young <byoung@bigbluehat.com>
2011-10-14 22:05:50 -0400

    merged in blogging branch

--- lists/page.js
+++ lists/page.js
@@ -1,50 +1,115 @@
 function(head, req) {
-	// !code lib/array_replace_recursive.js
-	var ddoc = this;
-	var templates = ddoc.templates;
-	var mustache = require("lib/mustache");
-	var row;
-	provides('html',
-		function() {
-			var page = {items:[],copyright:'BigBlueHat'};
-			while(row = getRow()) {
-				var last = row.key.pop();
-				var secondtolast = row.key.pop();
-				// it's the page
-				if (secondtolast == '_' && last == '_') {
-					page.title = row.doc.title;
-				} else if (secondtolast == '' && last == 'site') {
-					page.site = row.doc;
-				} else if (secondtolast == '' && last == 'sitemap') {
-					page.sitemap = row.doc.urls;
-				} else if (secondtolast == '' && last == 'template') {
-					templates = array_replace_recursive(ddoc.templates, row.doc.templates);
-				} else {
-					// TODO: base template selection off type
-					if (!page.items[secondtolast]) page.items[secondtolast] = {'area':[]};
-					if (row.doc.type) {
-						if (row.doc.type == 'navigation') {
-							var navigation = {'sitemap':{}};
-							if (row.doc.show_only && row.doc.show_only == 'children') {
-								// TODO: this needs to be recursive
-								page.sitemap.forEach(function(el) {
-									if (el.body.url == row.doc.current_url && el.children) {
-										navigation.sitemap = el.children;
-									}
-								});
-							} else {
-								navigation.sitemap = page.sitemap;
-							}
-							page.items[secondtolast].area[last] = {'item':mustache.to_html(templates.types[row.doc.type], navigation, templates.partials)};
-						} else {
-							page.items[secondtolast].area[last] = {'item':mustache.to_html(templates.types[row.doc.type], row.doc)};
-						}
-					}
-					if (secondtolast == 0) page.items[secondtolast].classes = ['first'];
-				}
-			}
-			page.site.host = req.headers.Host;
-			send(mustache.to_html(templates.page, page, templates.partials));
-		}
-	);
+  // !code lib/array_replace_recursive.js
+  var ddoc = this,
+      templates = ddoc.templates,
+      mustache = require("lib/mustache"),
+      row;
+
+  if (!req.query && !req.query.include_docs) {
+    send('Please add include_docs to make this thing work');
+  }
+
+  provides('html',
+    function() {
+      var page = {items:[],copyright:'BigBlueHat'},
+          posts = [],
+          navigation = [],
+          is_post = false;
+      while(row = getRow()) {
+        var last = row.key.pop();
+        if (last == 'post') {
+          is_post = true;
+          last = row.key.pop();
+        }
+        var secondtolast = row.key.pop();
+        // it's the page
+        if (secondtolast == '_' && last == '_') {
+          page.title = row.doc.title;
+        } else if (secondtolast == '' && last == 'site') {
+          page.site = row.doc;
+        } else if (secondtolast == '' && last == 'sitemap') {
+          page.sitemap = row.doc.urls;
+        } else if (secondtolast == '' && last == 'template') {
+          templates = array_replace_recursive(ddoc.templates, row.doc.templates);
+        } else {
+          // TODO: base template selection off type
+          if (!page.items[secondtolast]) {
+            page.items[secondtolast] = {'area':[]};
+          }
+          if (row.doc.type) {
+            if (row.doc.type === 'navigation') {
+              navigation.push(page.items[secondtolast].area[last] = row);
+            } else {
+              var doc = row.doc;
+              // display settings
+              if (row.value.display_title === false) {
+                doc.title = "";
+              }
+              if (is_post) {
+                // blog posts handling
+                if (page.items[secondtolast].area[last] === undefined) {
+                  page.items[secondtolast].area[last] = {'posts':[]};
+                  //posts = page.items[secondtolast].area[last];
+                }
+                page.items[secondtolast].area[last]['posts']
+                  .push({'item':mustache.to_html(templates.types[row.doc.type], doc),
+                	    'published_date':row.value.published_date.join('/')});
+              } else {
+                // non-post item
+                page.items[secondtolast].area[last] = {'item':mustache.to_html(templates.types[row.doc.type], doc)};
+              }
+            }
+          }
+          // TODO: fix .first assignment
+          if (secondtolast === 0) {
+        	    page.items[secondtolast].classes = ['first'];
+          }
+        }
+        is_post = false;
+      } // end while
+
+      // find navigation items and generate their content
+      page.items.forEach(function(area, area_idx) {
+        area.area.forEach(function(item, idx) {
+          if (item.doc && item.doc.type && item.doc.type == 'navigation') {
+            var nav_item = item,
+                nav = {'sitemap':{}};
+
+            if (nav_item.doc.show_only && nav_item.doc.show_only == 'children') {
+              // TODO: this needs to be recursive
+              page.sitemap.forEach(function(el) {
+                if (el.body.url == nav_item.doc.current_url && el.children) {
+                  navigation.sitemap = el.children;
+                }
+              });
+            } else {
+              navigation.sitemap = page.sitemap;
+            }
+            page.items[area_idx].area[idx] = {'item':mustache.to_html(templates.types['navigation'], navigation, templates.partials)};
+          }
+        });
+      });
+      page.site.host = req.headers.Host;
+
+      send(mustache.to_html(templates.page, page, templates.partials));
+    }
+  );
 }
+
+/*
+
+            if (row.doc.type == 'navigation') {
+              var navigation = {'sitemap':{}};
+              if (row.doc.show_only && row.doc.show_only == 'children') {
+                // TODO: this needs to be recursive
+                page.sitemap.forEach(function(el) {
+                  if (el.body.url == row.doc.current_url && el.children) {
+                    navigation.sitemap = el.children;
+                  }
+                });
+              } else {
+                navigation.sitemap = page.sitemap;
+              }
+              page.items[secondtolast].area[last] = {'item':mustache.to_html(templates.types[row.doc.type], navigation, templates.partials)};
+            } else {
+*/
--- views/page_and_items/map.js
+++ views/page_and_items/map.js
@@ -1,16 +1,48 @@
 function(doc) {
-	if (doc.type == 'page') {
-		var urlParts = doc.url.split('/');
-		for (var i in doc.page_items) {
-			for (var id in doc.page_items[i]) {
-				emit(urlParts.concat(i, id), doc.page_items[i][id]);
-			}
-		}
-		emit(urlParts.concat('_', '_'), {'_id': doc._id});
-		if (doc.template) {
-			emit(urlParts.concat('', 'template'), {'_id':doc.template});
-		}
-		emit(urlParts.concat('', 'site'), {'_id':'site'});
-		emit(urlParts.concat('', 'sitemap'), {'_id':'sitemap'});
-	}
+  // TODO: can we put this someplace "global" like a site-wide setting?
+  var max_posts = 10,
+      urlParts, i, n, post, post_count, posts;
+
+  function sortByPublishedDate(a, b) {
+    var ad = new Number(a.published_date.join('')),
+        bd = new Number(b.published_date.join(''));
+    if (ad < bd) {
+      return 1;
+    } else {
+      return -1;
+    }
+    return 0;
+  }
+
+  if (doc.type == 'page') {
+    urlParts = doc.url.split('/');
+    for (i in doc.page_items) {
+      for (n in doc.page_items[i]) {
+        if (doc.page_items[i][n].posts != undefined) {
+          post_count = 0;
+          posts = [];
+          for (post in doc.page_items[i][n].posts) {
+            if (post_count == max_posts) {
+              break;
+            }
+            posts.push(doc.page_items[i][n].posts[post]);
+            post_count++;
+          }
+          posts.sort(sortByPublishedDate);
+          posts.forEach(function(post) {
+            emit(urlParts.concat(i, n, 'post'), post);
+          });
+          post_count = 0; // in case posts exist in other areas
+        } else {
+          emit(urlParts.concat(i, n), doc.page_items[i][n]);
+        }
+      }
+    }
+    emit(urlParts.concat('_', '_'), {'_id': doc._id});
+    if (doc.template) {
+      emit(urlParts.concat('', 'template'), {'_id':doc.template});
+    }
+    emit(urlParts.concat('', 'site'), {'_id':'site'});
+    emit(urlParts.concat('', 'sitemap'), {'_id':'sitemap'});
+  }
 }
