024eb12c90638244f1a11ef8acf3349007234774
Benjamin Young <byoung@bigbluehat.com>
2011-10-26 21:17:05 -0400

    added post handling to items_published_on MapReduce

--- views/items_published_on/map.js
+++ views/items_published_on/map.js
@@ -1,11 +1,17 @@
 function(doc) {
-	if (doc.type != 'page') {
-		emit(doc._id, doc);
-	} else if (doc.type == 'page') {
-		for (var i in doc.page_items) {
-			for (var id in doc.page_items[i]) {
-				emit(doc.page_items[i][id]._id, {'published_on':doc._id});
-			}
-		}
-	}
+  if (doc.type !== 'page') {
+    emit(doc._id, doc);
+  } else if (doc.type === 'page') {
+    for (var i in doc.page_items) {
+      for (var id in doc.page_items[i]) {
+        if (doc.page_items[i][id].posts) {
+          for (var p in doc.page_items[i][id].posts) {
+            emit(doc.page_items[i][id].posts[p]._id, {'published_on':doc._id}); 
+          }
+        } else {
+          emit(doc.page_items[i][id]._id, {'published_on':doc._id});
+        }
+      }
+    }
+  }
 }
