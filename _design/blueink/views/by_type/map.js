6890dce092d06f9ad230ea6b86ebe7185df9cc9d
BigBlueHat <byoung@bigbluehat.com>
2014-12-24 09:14:53 -0700

    Broaden type listing
    Name vs. Title. More stupidness from the
    data formats war...

--- _design/blueink/views/by_type/map.js
+++ _design/blueink/views/by_type/map.js
@@ -1,6 +1,7 @@
 function(doc) {
-  if ('type' in doc && 'title' in doc
-      && doc.type !== 'page' && doc.type !== 'site') {
+  if ('type' in doc && ('title' in doc || 'name' in doc)
+      && doc.type !== 'page' && doc.type !== 'site'
+      && doc.type !== 'template') {
     emit(doc.type, doc.title);
   }
 }
