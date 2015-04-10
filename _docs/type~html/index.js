7bfa4838b295bb4b7101c60fb64adb87c3d3da0d
BigBlueHat <byoung@bigbluehat.com>
2015-04-09 20:35:27 -0400

    Add type~html
    Likely the name should change to `text`
    or `prose` or somesuch...right now it
    matches `widget` for all intents and purposes.
    
    In the old BlueInk the `html` type was a
    TinyMCE enhanced editing space for the
    typical content of a text-focused site.
    
    It was also referred to as `text` in the UI.
    
    So...

---
+++ _docs/type~html/index.js
@@ -0,0 +1,19 @@
+Vue.component('html-editor', {
+  template: require('./editor.html'),
+  replace: true,
+  data: function() {
+    return {
+      doc: {
+        title: "",
+        content: ""
+      }
+    }
+  },
+  methods: {
+    output: function() {
+      this.doc.type = 'html';
+      // TODO: add created & updated
+      return this.doc;
+    }
+  }
+});
