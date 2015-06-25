ff4038b12d966bdbf26af201b9349467a2ab53fb
BigBlueHat <byoung@bigbluehat.com>
2015-06-25 09:27:28 -0400

    Handle collections with specified types
    Product lists, galleries, etc.

--- src/main.js
+++ src/main.js
@@ -37,6 +37,19 @@ window.page = page = new BlueInk({
       } else {
         return false;
       }
+    },
+    collection_info: function() {
+      var collection_info = {};
+      // TODO: >_< rather expensive looping -- turn into a $watch + deep watching
+      this.page.page_items.forEach(function(area) {
+        area.forEach(function(item) {
+          if (undefined !== item._collection
+              && Object.keys(item._collection).length > 0) {
+            collection_info = item._collection;
+          }
+        });
+      });
+      return collection_info;
     }
   },
   watch: {
--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -39,6 +39,39 @@ module.exports = {
         location.reload();
       });
     },
+    addToCollection: function(type) {
+      var collection_info = this.$root.collection_info;
+      var allowed_type = '';
+      if (undefined !== collection_info.type) {
+        allowed_type = collection_info.type;
+      }
+      // TODO: support multiple types?
+      if (type !== allowed_type) {
+        return false;
+      }
+
+      // TODO: expose allowed_type to UI to prevent the button being available
+
+      var item = {
+        _id: this.selected
+        // TODO: also add date info about when this was added to the page
+      };
+
+      if (undefined !== this.$root.page.collection) {
+        this.$root.page.collection.push(item);
+      } else {
+        // add to area 0 as item 0
+        this.$root.page.collection = [item];
+      }
+      this.$root.savePage(function() {
+        location.reload();
+      });
+    },
+    canHazCollection: function(type) {
+      return (undefined !== this.$root.page.collection
+              && undefined !== this.$root.collection_info.type
+              && type === this.$root.collection_info.type);
+    },
     loadPreview: function() {
       var self = this;
       // TODO: implement an actual preview
