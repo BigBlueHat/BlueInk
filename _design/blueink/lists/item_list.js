048acb62fff820de30607d32112b85e1a4de18f3
BigBlueHat <byoung@bigbluehat.com>
2015-11-12 21:15:51 -0500

    Fix _blueink/item-types & _blueink/items-by-type endpoints

--- _design/blueink/lists/item_list.js
+++ _design/blueink/lists/item_list.js
@@ -1,16 +1,13 @@
 function(head, req) {
   var row;
-  var types = Object.keys(this.templates.types);
 
   provides('html',
     function() {
       send('<ul>');
       while(row = getRow()) {
-        if (types.indexOf(row.key) !== -1) {
-          send('<li data-blueink-type="' + row.key + '">'
-            + '<a href="' + row.id + '" title="' + row.value + '">'
-            + row.value + '</a></li>');
-        }
+        send('<li data-blueink-type="' + row.key + '">'
+          + '<a href="' + row.id + '" title="' + row.value + '">'
+          + row.value + '</a></li>');
       }
       send('</ul>');
     }
--- _design/blueink/lists/types_list.js
+++ _design/blueink/lists/types_list.js
@@ -1,17 +1,14 @@
 function(head, req) {
   var row;
-  var types = Object.keys(this.templates.types);
 
   provides('html',
     function() {
       send('<ul>');
       while(row = getRow()) {
-        if (types.indexOf(row.key) !== -1) {
-          send('<li data-blueink-type="' + row.key + '">'
-            + '<a href="items-by-type?key=%22' + row.key + '%22" title="' + row.key + '">'
-            + row.key + '</a>'
-            + ' <span class="count">' + row.value + '</span></li>');
-        }
+        send('<li data-blueink-type="' + row.key + '">'
+          + '<a href="items-by-type?key=%22' + row.key + '%22" title="' + row.key + '">'
+          + row.key + '</a>'
+          + ' <span class="count">' + row.value + '</span></li>');
       }
       send('</ul>');
     }
