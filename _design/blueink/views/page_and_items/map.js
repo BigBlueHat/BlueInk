51216e011e06c7e7d7d08ce6330b4bf88b7882eb
BigBlueHat <byoung@bigbluehat.com>
2015-12-15 11:05:06 -0500

    Support the new collection object format
    Simpler, more flexible, more awesomer

--- _design/blueink/views/page_and_items/map.js
+++ _design/blueink/views/page_and_items/map.js
@@ -33,6 +33,36 @@ function(doc) {
             emit(urlParts.concat(item._id, '', '0', '0'),
                  {'_id': item._id});
           });
+        } else if (typeof doc.page_items[i][n]['_id'] !== 'string'
+            && '$ref' in doc.page_items[i][n]['_id']
+            && doc.page_items[i][n]['_id']['$ref'] === '#/collection') {
+          // check for JSON Reference
+          // http://tools.ietf.org/html/draft-pbryan-zyp-json-ref-03#section-3
+          // value is a JSON Pointer
+          // http://tools.ietf.org/html/rfc6901#section-6
+
+          // if we've got one of these, then `collection` has all the goods
+          // TODO: ...lots of work to avoid the `items` list being in the index
+          var collection_meta = {
+            title: doc.collection.title,
+            type: doc.collection.type,
+            template_type: doc.collection.template_type
+          };
+          emit(urlParts.concat('', i, n, '_collection'),
+               {"_collection": collection_meta});
+
+          // collections have items; loop 'em!
+          doc.collection.items.forEach(function(item, idx) {
+            emit(urlParts.concat('', i, n, 'item', idx), item);
+            // emit item id as child URL of the collection page
+            emit_meta(urlParts.concat(item._id));
+            // emit collection page as the "page"
+            emit(urlParts.concat(item._id, '', '_', '_'),
+                 {'_id': doc._id});
+            // and the actual item as the only item on the page
+            emit(urlParts.concat(item._id, '', '0', '0'),
+                 {'_id': item._id});
+          });
         } else {
           emit(urlParts.concat('', i, n), doc.page_items[i][n]);
         }
