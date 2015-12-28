aa0dbceb3aad90b05d6cfd3d7b87da1578a8f37c
BigBlueHat <byoung@bigbluehat.com>
2015-12-27 21:26:23 -0500

    Match sitemap to new collection style; add redirect

--- _design/blueink/views/pages/map.js
+++ _design/blueink/views/pages/map.js
@@ -1,9 +1,22 @@
 function(doc) {
   if (doc.type === 'page') {
-    emit(doc._id.split('/'),
-         {
-           nav_label: doc.nav_label,
-           is_collection: ('collection' in doc && doc.collection.length > 0)
-         });
+
+    var is_collection = false;
+    if ('collection' in doc) {
+      if (doc.collection.length > 0) {
+        // old collection style
+        is_collection = true;
+      } else if ('items' in doc.collection
+          && doc.collection.items.length > 0) {
+        // new collection style
+        is_collection = true;
+      }
+    }
+
+    emit(doc._id.split('/'), {
+      is_collection: is_collection,
+      nav_label: doc.nav_label,
+      redirect: doc.redirect
+    });
   }
 }
