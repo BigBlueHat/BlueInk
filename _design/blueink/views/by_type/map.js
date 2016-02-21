570c5411a9c5d4dac125d832ed0a910e32eaa83e
BigBlueHat <byoung@bigbluehat.com>
2016-02-21 01:19:18 -0500

    Output URLs of image type documents (they have no title)

--- _design/blueink/views/by_type/map.js
+++ _design/blueink/views/by_type/map.js
@@ -1,5 +1,11 @@
 function(doc) {
-  if ('type' in doc && ('title' in doc || 'name' in doc)) {
-    emit(doc.type, doc.title || doc.name);
+  if ('type' in doc) {
+    if ('title' in doc || 'name' in doc) {
+      emit(doc.type, doc.title || doc.name);
+    } else if ('self' in doc._attachments
+        && doc._attachments['self'].content_type.substr(0, 6) === 'image/') {
+      // image types don't have titles, so generate their src's
+      emit(doc.type, '_blueink/' + doc._id + '/self');
+    }
   }
 }
