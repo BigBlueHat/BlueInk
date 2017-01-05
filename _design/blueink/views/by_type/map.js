24a964f2fa3bf310c9bd28efa5507622bba7870b
BigBlueHat <byoung@bigbluehat.com>
2017-01-05 14:12:23 -0500

    Fix _attachments checking in by_type map

--- _design/blueink/views/by_type/map.js
+++ _design/blueink/views/by_type/map.js
@@ -2,10 +2,13 @@ function(doc) {
   if ('type' in doc) {
     if ('title' in doc || 'name' in doc) {
       emit(doc.type, doc.title || doc.name);
-    } else if ('self' in doc._attachments
+    } else if ('_attachments' in doc
+        && 'self' in doc._attachments
         && doc._attachments['self'].content_type.substr(0, 6) === 'image/') {
       // image types don't have titles, so generate their src's
       emit(doc.type, '_blueink/' + doc._id + '/self');
+    } else if (doc.type === 'sitemap') {
+      emit(doc.type, 'Sitemap');
     }
   }
 }
